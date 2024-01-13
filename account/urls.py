acc3e69e913912028a467ab1c4fb5c1fcb4d7c8c
ArthurAb <abgaryanarthurelina@gmail.com>
2024-01-13 17:51:32 +0300

    доделали редактирование профиля

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from .views import login_auth
 from django.contrib.auth import views as auth_view
-from .views import main, profile_register
+from .views import main, profile_register,edit
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # path('', login_auth, name = 'login_auth'),
-    path('', auth_view.LoginView.as_view(), name='login'), #обработчик входа
+    path('main/', main, name='main'),
+
+    path('account/login/', auth_view.LoginView.as_view(), name='login'), #обработчик входа
     path('account/logout/', auth_view.LogoutView.as_view(), name='logout'),#обработчик выхода
     path('password_change/',auth_view.PasswordChangeView.as_view(), name= 'password_change'),#пров-т форму смены пароля
     path('password_change/done/',auth_view.PasswordChangeDoneView.as_view(), name = 'password_change_done'),#возв-т соош-е об успешной отп-ке
@@ -14,6 +18,11 @@ urlpatterns = [
     path('password_confirm/<uidb64>/<token>/',auth_view.PasswordResetConfirmView.as_view(), name = 'password_reset_confirm'),#пров-т отправленную ссылку для вос-ия пароля и возврашает форму заполнения нового пароля
     path('password_reset_complite/', auth_view.PasswordResetCompleteView.as_view(), name = 'password_reset_complete'),#ссылка успешной смены пароля
     path('register/',profile_register, name = 'register' ),
-    path('main/',main, name = 'main')
+    path('account/edit/',edit, name = 'edit'),
 
 ]
+
+
+if settings.DEBUG:
+    urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- account/views.py
+++ account/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
-from .forms import LoginForms,UserRegistrationForm
+from .forms import LoginForms,UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .models import Profile
+from django.contrib.auth.models import User
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data = request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile,data =  request.POST, files = request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        # user_post = User.objects.get(user = request.user)
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(request, 'account/edit.html',{'user_form':user_form,'profile_form':profile_form})
 
 def profile_register(request):
     if request.method == 'POST':
@@ -10,8 +26,9 @@ def profile_register(request):
         if form.is_valid():
             new_user = form.save(commit=False)#сохрн. нового поль-ля но не заносим в базу
             new_user.set_password(form.cleaned_data['password'])#присваиваем пол-ль зашифрован пароль
-            new_user.save()#созраняем в базу
-            return render(request, 'account/register_done.html',{'new_user':new_user})#при успешной регистрации
+            new_user.save()#сохраняем в базу
+            Profile.objects.create(user = new_user)
+            return render(request, 'account/register_done.html',{'new_user':new_user,})#при успешной регистрации
     else:
         form = UserRegistrationForm()
     return render (request, 'account/register.html', {'form':form})
@@ -37,4 +54,4 @@ def login_auth(request):
 
 @login_required
 def main(request):
-    return render(request, 'account/main.html',{'select':'Успешно'})
+    return render(request, 'account/main.html',{'select':'Это текст главной страницы'})
